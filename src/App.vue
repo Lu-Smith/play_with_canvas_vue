1fb6a3b75f64ea6e0ca73ee1c67facec60c1602f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-08 19:29:37 +0000

    Added Canvas One and CanvasTwo to App
    Removed HelloWorld.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import CanvasOne from './components/CanvasOne.vue';
+import CanvasTwo from './components/CanvasTwo.vue';
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    Hello World
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <CanvasOne msg="Vite + Vue" />
+  <CanvasTwo msg="Vite + Vue" />
 </template>
 
 <style scoped>
