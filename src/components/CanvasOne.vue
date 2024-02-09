18ee6ee53d82130a058b25323e1c7b08096f04b6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-09 18:35:20 +0000

    Updated CanvasOne.vue

--- src/components/CanvasOne.vue
+++ src/components/CanvasOne.vue
@@ -1,48 +1,43 @@
 <template>
-   <h2>{{ msg }}</h2>
-   <h2>one</h2>
-   <canvas ref="canvasOne"></canvas>
-</template>
-
-<script setup lang="ts">
-import { ref, onMounted } from 'vue';
-
-defineProps<{ msg: string }>();
-
-const canvasOne = ref<HTMLCanvasElement | null>(null);
-
-const handleKeyDown = (event: KeyboardEvent) => {
-    switch (event.key) {
-            case "ArrowUp":
-            if (event.repeat) {
-                return;
-            }
-            break;
-    }
-};
-
-const handleMouseEvent = (event: MouseEvent) => {
-    if (event) {
-    }
-};
-
-const updateGame = () => {
-    const context = canvasOne.value?.getContext('2d');
-
-    if (context && canvasOne.value) {
-        // Clear the canvas
-        context.clearRect(0, 0, canvasOne.value.width, canvasOne.value.height);
-
-        // Draw ground
-        context.fillStyle = '#000';
-        context.fillRect(10, 10, 40, 40);
-    }
-}
-
-onMounted(() => {
-    window.addEventListener('keydown', handleKeyDown);
-    window.addEventListener('mousedown', handleMouseEvent);
-    updateGame();
-});
-
-</script>
+    <h2>{{ msg }}</h2>
+    <canvas ref="canvasOne"></canvas>
+ </template>
+ 
+ <script setup lang="ts">
+ import { ref, onMounted, defineProps } from 'vue';
+ 
+ const canvasOne = ref<HTMLCanvasElement | null>(null);
+ 
+ const handleKeyDown = (event: KeyboardEvent) => {
+     switch (event.key) {
+         case "ArrowUp": 
+             updateGame();
+             break;
+     }
+ };
+ 
+ const handleMouseEvent = (event: MouseEvent) => {
+     if (event) {
+         updateGame();
+     }
+ };
+ 
+ const updateGame = () => {
+     const context = canvasOne.value?.getContext('2d');
+ 
+     if (context && canvasOne.value) {
+         // Clear the canvas
+         context.clearRect(0, 0, canvasOne.value.width, canvasOne.value.height);
+ 
+         // Draw ground
+         context.fillStyle = '#fff';
+         context.fillRect(10, 10, 40, 40);
+     }
+ }
+ 
+ onMounted(() => {
+     window.addEventListener('keydown', handleKeyDown);
+     window.addEventListener('mousedown', handleMouseEvent);
+ });
+ 
+ </script>
