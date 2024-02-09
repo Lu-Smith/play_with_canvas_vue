5d1cc73b488865db00d15863dc42f636696c3f5b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-09 18:12:38 +0000

    Updated CanvasOne.vue

--- src/components/CanvasOne.vue
+++ src/components/CanvasOne.vue
@@ -1,8 +1,27 @@
 <template>
    <h2>{{ msg }}</h2>
    <h2>one</h2>
+   <canvas ref="canvasOne"></canvas>
 </template>
 
 <script setup lang="ts">
-defineProps<{ msg: string }>()
+import { ref, onMounted } from 'vue';
+
+defineProps<{ msg: string }>();
+
+const canvasOne = ref<HTMLCanvasElement | null>(null);
+
+const handleKeyDown = () => {
+
+};
+
+const handleMouseEvent = () => {
+
+};
+
+onMounted(() => {
+        window.addEventListener('keydown', handleKeyDown);
+        window.addEventListener('mousedown', handleMouseEvent);
+});
+
 </script>
