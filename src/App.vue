8f662f07092d76fff4b57d9a3a53d494fb5b5cad
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-11 16:11:49 +0000

    Added CanvasTop to App

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import CanvasOne from './components/CanvasOne.vue';
+import CanvasTop from './components/CanvasTop.vue';
 import CanvasTwo from './components/CanvasTwo.vue';
 import Footer from './components/Footer.vue';
 </script>
 
 <template>
+  <div class="headerContainer">
+    <CanvasTop />
+  </div>
   <h1>Play with Canvas</h1>
   <div class="canvasContainer">
     <CanvasOne msg="Dots" />
