698d198a30f900852096c52e138b09867a8d73c5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-16 16:19:49 +0000

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import Footer from './components/Footer.vue';
   <div class="headerContainer">
     <CanvasTop />
   </div>
-  <h1>Play with Canvas</h1>
+  <h1>Play with <span>Canvas</span></h1>
   <div class="canvasContainer">
     <CanvasOne />
     <CanvasTwo />
@@ -19,6 +19,9 @@ import Footer from './components/Footer.vue';
     <CanvasFour />
   </div>
   <Footer />
+  <div class="headerContainer">
+    <CanvasTop />
+  </div>
 </template>
 
 <style scoped>
