8df62440ea946360ed04aadabb60a9dc1ea16653
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-16 16:21:06 +0000

    Updated Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,9 @@
 <template>
-    Coded by 
-    <a href="https://www.lunasmithart.com/" target="_blank" rel="noreferrer">Luna Smith</a>
-    -
-    <a href="https://github.com/Lu-Smith/play_with_canvas_vue" target="_blank" rel="noreferrer">GitHub</a>
-    .
+    <h4>
+        Coded by 
+        <a href="https://www.lunasmithart.com/" target="_blank" rel="noreferrer">Luna Smith</a>
+        -
+        <a href="https://github.com/Lu-Smith/play_with_canvas_vue" target="_blank" rel="noreferrer">GitHub</a>
+        .
+    </h4>
 </template>
