f2c97d98cae9ecb7576d6f0ee712a26b821f1a7a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-16 12:44:19 +0000

    Updated CanvasOne.vue

--- src/components/CanvasOne.vue
+++ src/components/CanvasOne.vue
@@ -74,6 +74,7 @@
             if (distance < 100 && context) {
                 context.beginPath();
                 context.strokeStyle = particleArray.value[i].color;
+                context.lineWidth = 0.5;
                 context.moveTo(particleArray.value[i].x, particleArray.value[i].y);
                 context.lineTo(particleArray.value[j].x, particleArray.value[j].y);
                 context.stroke();
