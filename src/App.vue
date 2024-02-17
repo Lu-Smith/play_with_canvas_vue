a1a44685de1ec65d0a70dabfd9056e83c995a34a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-17 11:43:27 +0000

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,37 +7,10 @@ import CanvasThree from './components/CanvasThree.vue';
 import CanvasFour from './components/CanvasFour.vue';
 import Footer from './components/Footer.vue';
 
-const canvasChoice1 = ref(true);
-const canvasChoice2 = ref(false);
-const canvasChoice3 = ref(false);
-const canvasChoice4 = ref(false);
+const selectedCanvas = ref(1);
 
-const chooseCanvas1 = () => {
-  canvasChoice1.value = true;
-  canvasChoice2.value = false;
-  canvasChoice3.value = false;
-  canvasChoice4.value = false;
-}
-
-const chooseCanvas2 = () => {
-  canvasChoice1.value = false;
-  canvasChoice2.value = true;
-  canvasChoice3.value = false;
-  canvasChoice4.value = false;
-}
-
-const chooseCanvas3 = () => {
-  canvasChoice1.value = false;
-  canvasChoice2.value = false;
-  canvasChoice3.value = true;
-  canvasChoice4.value = false;
-}
-
-const chooseCanvas4 = () => {
-  canvasChoice1.value = false;
-  canvasChoice2.value = false;
-  canvasChoice3.value = false;
-  canvasChoice4.value = true;
+const chooseCanvas = (canvasNumber: number) => {
+  selectedCanvas.value = canvasNumber;
 }
 
 </script>
@@ -47,21 +20,21 @@ const chooseCanvas4 = () => {
     <CanvasTop />
   </div>
   <h1>Play with <span>Canvas</span></h1>
-  <button @click="chooseCanvas1">1</button>
-  <button @click="chooseCanvas2">2</button>
-  <button @click="chooseCanvas3">3</button>
-  <button @click="chooseCanvas4">4</button>
+  <button @click="chooseCanvas(1)">1</button>
+  <button @click="chooseCanvas(2)">2</button>
+  <button @click="chooseCanvas(3)">3</button>
+  <button @click="chooseCanvas(4)">4</button>
   <div class="canvasContainer">
-    <div v-if="canvasChoice1">
+    <div v-if="selectedCanvas === 1">
       <CanvasOne />
     </div>
-    <div v-if="canvasChoice2">
+    <div v-if="selectedCanvas === 2">
       <CanvasTwo />
     </div>
-    <div v-if="canvasChoice3">
+    <div v-if="selectedCanvas === 3">
       <CanvasThree />
     </div>
-    <div v-if="canvasChoice4">
+    <div v-if="selectedCanvas === 4">
       <CanvasFour />
     </div>
   </div>
