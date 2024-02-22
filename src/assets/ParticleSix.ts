e0b63cae766298831eaf89a15f1acb557a868a64
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-22 15:27:06 +0000

    Created ParticleSix.ts

---
+++ src/assets/ParticleSix.ts
@@ -0,0 +1,43 @@
+export class ParticleSix {
+    x: number;
+    y: number;
+    size: number;
+    speedX: number;
+    speedY: number;
+    color: string;
+
+    constructor(mouseX: number, mouseY: number, hue: number){
+        this.x = mouseX;
+        this.y = mouseY;
+        this.size = Math.random() * 40 + 1;
+        this.speedX = Math.random() * 6 - 1.5;
+        this.speedY = Math.random() * 6 - 1.5;
+        this.color = '#79c2d0';
+    }
+
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if ( this.size > 0.2) {
+            this.size -= 0.1
+        }
+        if ( this.size < 30 && this.size > 15) {
+            this.color = '#ffc93c'
+        } else {
+            this.color = '#79c2d0';
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        //Draw particles
+        if (context) {
+            context.fillStyle = this.color;
+            context.fillRect(this.x, this.y, this.size, this.size);
+
+        //stroke
+            context.strokeStyle = 'white'; 
+            context.lineWidth = 4;
+            context.strokeRect(this.x, this.y, this.size, this.size);  
+        }
+    }
+}
